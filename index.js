d364202828a8b59e98c597db754315b08ee98571
Jessica Gladstone <jcg321@comcast.net>
2019-04-29 21:29:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function overwriteBestCustomer() {
 const leastFavoriteCustomer = 'George';
 
 function changeLeastFavoriteCustomer() {
-  const leastFavoriteCustomer = 'Sally';
+leastFavoriteCustomer = 'Sally';
 }
